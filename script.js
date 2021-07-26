4b87737674c5734747239fa17143d746b5043ace
joaoritter8 <joaoritter8@gmail.com>
2021-07-26 19:56:18 -0400

    criaçao dos movimentos

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -21,5 +23,64 @@ function criarSnake(){
 
 }
 
-criarBG();
-criarSnake();
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right"){
+        direction = "left";
+    }
+    if(event.keyCode == 38 && direction != "down"){
+        direction = "up";
+    }
+    if(event.keyCode == 39 && direction != "left" ){
+        direction = "right";
+    }
+    if(event.keyCode == 40 && direction != "up"){
+        direction = "down";
+    }
+}
+
+function startGame(){
+    
+    if(snake[0].x >15 * box && direction == "right"){
+        snake[0].x = 0;
+    }
+    if(snake[0].x < 0 && direction == "left"){
+        snake[0].x = 16 * box;
+    }
+    if(snake[0].y > 15 *box && direction == "down"){
+        snake[0].y = 0;
+    }
+    if(snake[0].y < 0 && direction == "up"){
+        snake[0].y = 16 * box;
+    }
+
+
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right"){
+        snakeX += box;
+    }else if(direction == "left"){
+        snakeX -= box;
+
+    }else if(direction == "up"){
+        snakeY -=box;
+    }else if(direction == "down"){
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
